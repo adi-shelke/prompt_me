4060471a462a9ff9e51f4d4eed4989f8cb9b604b
Adinath Shelke <shelkeadinath3@gmail.com>
2023-07-28 14:43:41 +0530

    coded edit endpoint

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,37 @@
+import { connectToDb } from "@utils/database";
+import Prompt from "@models/prompt";
+
+//for GET request
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDb();
+    const prompt = await Prompt.findById(params.id).populate("creator");
+    if (!prompt) return new Response("Prompt not found", { status: 404 });
+    return new Response(JSON.stringify(prompt), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response("Failed to load the prompts", {
+      status: 500,
+    });
+  }
+};
+
+export const PATCH = async (req, { params }) => {
+  const { prompt, tag } = await req.json();
+  try {
+    await connectToDb();
+    const currentPrompt = await Prompt.findById(params.id);
+
+    if (!currentPrompt)
+      return new Response("Prompt not found", { status: 404 });
+
+    currentPrompt.prompt = prompt;
+    currentPrompt.tag = tag;
+    await currentPrompt.save();
+
+    return new Response(JSON.stringify(currentPrompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to update prompt", { status: 500 });
+  }
+};
