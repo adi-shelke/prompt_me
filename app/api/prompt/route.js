86b5954c9fde1ef7d1cff7aa27d255e92f0ded09
Adinath Shelke <shelkeadinath3@gmail.com>
2023-07-28 22:44:43 +0530

    refactored fetch prompts

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,16 +1,14 @@
-import { connectToDb } from "@utils/database";
 import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
 
-export const GET = async (req) => {
+export const GET = async (request) => {
   try {
-    await connectToDb();
+    await connectToDB();
+
     const prompts = await Prompt.find({}).populate("creator");
-    return new Response(JSON.stringify(prompts), {
-      status: 200,
-    });
+
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
-    return new Response("Failed to load the prompts", {
-      status: 500,
-    });
+    return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
