831ded1b24155f45d088d84b3a6474c98cf7f35e
Adinath Shelke <shelkeadinath3@gmail.com>
2023-07-28 22:25:56 +0530

    added cache in the create prompt page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -17,6 +17,7 @@ const CreatePrompt = () => {
     try {
       const response = await fetch("/api/prompt/new", {
         method: "POST",
+        cache: "no-store",
         body: JSON.stringify({
           prompt: post.prompt,
           userId: session?.user.id,
