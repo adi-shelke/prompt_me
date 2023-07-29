afb8106d38e7c02661a8b78a24c88ecfda1b7c9d
Adinath Shelke <shelkeadinath3@gmail.com>
2023-07-29 13:48:37 +0530

    auth route updated

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,26 +1,32 @@
-import NextAuth from "next-auth/next";
-import GooglePrvider from "next-auth/providers/google";
-import { connectToDb } from "@utils/database";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
 import User from "@models/user";
+import { connectToDB } from "@utils/database";
+
 const handler = NextAuth({
   providers: [
-    GooglePrvider({
+    GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   callbacks: {
     async session({ session }) {
-      const user = await User.findOne({ email: session.user.email });
-      session.user.id = user._id.toString();
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+
       return session;
     },
-    async signIn({ profile }) {
+    async signIn({ account, profile, user, credentials }) {
       try {
-        await connectToDb();
+        await connectToDB();
 
-        // check if the user already exist
+        // check if user already exists
         const userExists = await User.findOne({ email: profile.email });
+
+        // if not, create a new document and save user in MongoDB
         if (!userExists) {
           await User.create({
             email: profile.email,
@@ -28,10 +34,10 @@ const handler = NextAuth({
             image: profile.picture,
           });
         }
-        // if user doesn't exist, then create and save in Database
 
         return true;
       } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
         return false;
       }
     },
