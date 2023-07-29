98253a5b6fda7953c4843257d446de8a7a2789e4
Adinath Shelke <shelkeadinath3@gmail.com>
2023-07-29 13:24:47 +0530

    consoling in prompt route

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,9 +4,8 @@ import { connectToDb } from "@utils/database";
 export const GET = async (request) => {
   try {
     await connectToDb();
-
     const prompts = await Prompt.find({}).populate("creator");
-
+    console.log(prompts);
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
